38953ed82ddfef75d3c545492dbb311a1f4c23ca
ZhaiFanhua <zhaifanhua@foxmail.com>
2022-08-10 00:26:33 +0800

    新增：自动导入；

--- zhaifanhuablog-manage/vite.config.ts
+++ zhaifanhuablog-manage/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, ConfigEnv, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }: ConfigEnv) => {
@@ -15,7 +17,29 @@ export default defineConfig(({ mode }: ConfigEnv) => {
     },
 
     /* more config */
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [],
+        // 自定引入 Vue VueRouter API,如果还需要其他的可以自行引入
+        imports: ["vue", "vue-router"],
+        // 调整自动引入的文件位置
+        dts: "src/type/auto-import.d.ts",
+        // 解决自动引入eslint报错问题 需要在eslintrc的extend选项中引入
+        eslintrc: {
+          enabled: true,
+          // 配置文件的位置
+          filepath: "./.eslintrc-auto-import.json",
+          globalsPropValue: true,
+        },
+      }),
+      Components({
+        resolvers: [
+          // 需要自动导入的组件
+        ],
+        dts: "src/type/components.d.ts",
+      }),
+    ],
     server: {
       proxy: {
         "/api": {
